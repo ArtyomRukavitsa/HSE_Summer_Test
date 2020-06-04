408b2c2ad2425ebfa42507beac17f89753f34500
Артем (Desktop) <57802035+ArtyomRukavitsa@users.noreply.github.com>
2020-06-05 00:57:52 +0300

    Работа с API без задержек. Обработка, если нет подключение к Интернету

--- HSETestWork/app/src/main/java/com/example/hsetestwork/MainActivity.kt
+++ HSETestWork/app/src/main/java/com/example/hsetestwork/MainActivity.kt
@@ -1,32 +1,41 @@
 package com.example.hsetestwork
 
+import android.app.ProgressDialog
+import android.content.Context
+import android.net.ConnectivityManager
 import android.os.AsyncTask
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import android.widget.AdapterView
 import android.widget.ArrayAdapter
 import android.widget.Spinner
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
 import java.net.URL
-import com.github.kittinunf.fuel.httpGet
-import com.github.kittinunf.result.Result
 
 
 class EqualException() : Exception() // Если элементы spinner-ов совпадают при конвертации
 class FormatException() : Exception() // Если при конвертации пустое поле ввода
 
 
-
+object NetworkManager {
+    fun isNetworkAvailable(context: Context): Boolean {
+        val cm = context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+        val networkInfo = cm.activeNetworkInfo
+        return networkInfo != null && networkInfo.isConnected
+    }
+}
 class MainActivity : AppCompatActivity() {
-    var From : String = ""
-    var To: String = ""
+    var From : String = "RUB"
+    var To: String = "USD"
     var Current: Double = 0.0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        progressBar.setVisibility(View.GONE)
         // access the items of the list
         val currencies = resources.getStringArray(R.array.currencies)
 
@@ -38,7 +47,7 @@ class MainActivity : AppCompatActivity() {
                 android.R.layout.simple_spinner_item, currencies
             )
             spinner.adapter = adapter
-
+            spinner.setSelection(0) // по дефолту ставлю рубли
             spinner.onItemSelectedListener = object :
                 AdapterView.OnItemSelectedListener {
                 override fun onItemSelected(parent: AdapterView<*>, view: View, position: Int, id: Long) {
@@ -57,8 +66,9 @@ class MainActivity : AppCompatActivity() {
                 this,
                 android.R.layout.simple_spinner_item, currencies
             )
-            spinner2.adapter = adapter
 
+            spinner2.adapter = adapter
+            spinner2.setSelection(1) // по дефолту ставлю доллары
             spinner2.onItemSelectedListener = object :
                 AdapterView.OnItemSelectedListener {
                 override fun onItemSelected(parent: AdapterView<*>, view: View, position: Int, id: Long) {
@@ -72,7 +82,9 @@ class MainActivity : AppCompatActivity() {
     }
 
     inner class doAsync() : AsyncTask<Double, Void, Double>() {
-        override fun doInBackground(vararg params: Double?): Double? {
+        lateinit var pd : ProgressDialog //= ProgressDialog(this@MainActivity)
+
+        override fun doInBackground(vararg params: Double?) : Double? {
             var coef : Double = 0.0
             val result = URL("https://api.exchangeratesapi.io/latest?base=$From").readText()
             var str = result.replace("{\"rates\":{", "")
@@ -82,61 +94,54 @@ class MainActivity : AppCompatActivity() {
             for (el in r) {
                 if (To in el) coef = el.split(':')[1].toDouble()
             }
+            Log.v("IN", "Cur=$Current; From=$From; To=$To")
             return coef
         }
 
         override fun onPreExecute() {
             super.onPreExecute()
+            pd=ProgressDialog.show(this@MainActivity, "", "Checking...", true,
+                false);
+           //progressBar.setVisibility(View.VISIBLE);
+           // getWindow().setFlags(
+            //    WindowManager.LayoutParams.FLAG_NOT_TOUCHABLE,
+            //    WindowManager.LayoutParams.FLAG_NOT_TOUCHABLE)
         }
 
-        override fun onPostExecute(result: Double){
+        override fun onPostExecute(result: Double?) {
             super.onPostExecute(result)
-            Current = result
+            pd.dismiss()
+            //progressBar.setVisibility(View.GONE);
+            //getWindow().clearFlags(WindowManager.LayoutParams.FLAG_NOT_TOUCHABLE);
+            //Current = result
+        }
+
+        override fun onProgressUpdate(vararg values: Void?) {
+            super.onProgressUpdate(*values)
         }
     }
- //   private fun setCurrent(str: String) {
-   //     Current = str.toDouble()
-    //}
-   // private fun findCoef() : Double {
-//        var global : String? = null;
-//        "https://api.exchangeratesapi.io/latest?base=$From".httpGet()
-//            .responseString { request, response, result ->
-//                when (result) {
-//                    is Result.Failure -> {
-//                        val ex = result.getException()
-//                    }
-//                    is Result.Success -> {
-//                        val data = result.get()
-//                        var str = data.replace("{\"rates\":{", "")
-//                        str = str.replace("}", "")
-//                        val r = str.split(',')
-//
-//                        for (el in r) {
-//                            if (To in el)  textView4.text = el.split(':')[1]
-//                        }
-//                    }
-//                }
-//            }
-//        return textView4.text.toString().toDouble()
-       // var coef : Double = 0.0
-        //doAsync {
 
-
-
-        //}.execute()
-        //return coef
-  //  }
 
     fun convert(view: View) {
         try {
-            var res : Double
+            if (NetworkManager.isNetworkAvailable(this@MainActivity))
+            {
             val from = editTextNumber.text.toString()
             if (from == "") throw FormatException()
             if (From == To) throw EqualException()
-            doAsync().execute()
-            res = from.toDouble() * Current
-            textView4.setText("Курс: 1 $From = $Current $To")
+            //Log.v("BEFORE", "Cur=$Current; From=$From; To=$To")
+            val my_object = doAsync()
+            my_object.execute()
+            val r = my_object.get()
+            //Log.v("AFTER", "Cur=$Current; From=$From; To=$To r=$r")
+            val res = from.toDouble() * r
+            textView4.setText("Курс: 1 $From = $r $To")
             Res.setText(String.format("%.2f", res))
+            }
+            else {
+                Toast.makeText(this@MainActivity, "Проверьте подключение к сети Интернет!", Toast.LENGTH_SHORT).show()
+            }
+
         }
         catch (e: FormatException) {
             val toast = Toast.makeText(this, "Ошибка: пустое поле ввода", Toast.LENGTH_SHORT)
